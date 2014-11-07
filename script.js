ba14973270cb508254e8423bf14685f86ddc2ac1
shoichitakahashi <shoichi03071225@gmail.com>
2014-11-08 01:16:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 kadai03
 =======
+@@ -0,0 +1,21 @@
+            $( window ).resize(function(){
+                //画面サイズ表示
+                $("#size").html(window.innerWidth);
+                $("#chromeIcon").css({"WebkitTransform":"rotate("+window.innerWidth+"deg)"});
+                $("#chromeIcon").css({"MozTransform":"rotate("+window.innerWidth+"deg)"});
+            });
+
+            $(function(){
+                var bg = ["","#eee","#34495E","#1ABC9C","#2ECC71","#3498DB","#9B59B6","#95A5A6","#F1C40F","#E67E22","#E74C3C","#BDC3C7","#34495E"];
+            
+                var i=0;
+    
+                $(".boxStyle").each(
+                        function(){
+                            $(this).css({"background":bg[i]}).fadeOut(0).delay(100*i).fadeIn(500);
+                            i++;
+                        }
+                );
+                
+            
+            });
